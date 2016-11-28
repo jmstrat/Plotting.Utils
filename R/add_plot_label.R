64c63a45ea32cd82c34dfb83089dcc6a40a69563
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2016-11-28 15:05:51 +0000

    Add alignment

--- R/add_plot_label.R
+++ R/add_plot_label.R
@@ -2,17 +2,24 @@
 #'
 #' @param xoffset Offset in inches from the left limit of the plot
 #' @param yoffset Offset in inches from the top limit of the plot
+#' @param align Align to the left or right of the plot
 #' @param label The label text
 #' @param ... Additional parameters passed to \code{\link{text}}
 #' @export
-add_plot_label <- function(xoffset=0,yoffset=0,label,...) {
+add_plot_label <- function(xoffset=0,yoffset=0,align='left',label,...) {
   xin=grconvertX(xoffset, from = "inches", to = "ndc")
   yin=grconvertY(yoffset, from = "inches", to = "ndc")
 
   fig=par('fig')
 
-  xpos=grconvertX(fig[[1]]+xin, from = "ndc", to = "user")
   ypos=grconvertY(fig[[4]]-yin, from = "ndc", to = "user")
-
-  text(xpos,ypos,label,pos=4,xpd=NA,offset=0)
+  if(align=='left') {
+    xpos=grconvertX(fig[[1]]+xin, from = "ndc", to = "user")
+    text(xpos,ypos,label,pos=4,xpd=NA,offset=0)
+  } else if(align=='right') {
+    xpos=grconvertX(fig[[2]]-xin, from = "ndc", to = "user")
+    text(xpos,ypos,label,pos=2,xpd=NA,offset=0)
+  } else {
+    stop('Align must be one of left / right')
+  }
 }
