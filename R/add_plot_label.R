e9217069fd0457a4dd9bfa709b76c809453bc0e8
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2016-12-12 16:09:05 +0000

    Add inside / outside flag, update alignment

--- R/add_plot_label.R
+++ R/add_plot_label.R
@@ -3,22 +3,31 @@
 #' @param xoffset Offset in inches from the left limit of the plot
 #' @param yoffset Offset in inches from the top limit of the plot
 #' @param align Align to the left or right of the plot
+#' @param inside Label should be inside the box? : c(x,y) where x,y are T/F (or just T/F)
 #' @param label The label text
 #' @param ... Additional parameters passed to \code{\link{text}}
 #' @export
-add_plot_label <- function(xoffset=0,yoffset=0,align='left',label,...) {
+add_plot_label <- function(label, xoffset=0.05,yoffset=0.05,align='left',inside=TRUE,...) {
   xin=grconvertX(xoffset, from = "inches", to = "ndc")
   yin=grconvertY(yoffset, from = "inches", to = "ndc")
 
-  fig=par('fig')
+  if(length(inside)==1) inside=c(inside,inside)
 
-  ypos=grconvertY(fig[[4]]-yin, from = "ndc", to = "user")
+  if(inside[[1]]) xcord='npc' else xcord='nfc'
+  if(inside[[2]]) ycord='npc' else ycord='nfc'
+
+  plt_ndc=c(grconvertX(0,from=xcord,to='ndc'),
+            grconvertX(1,from=xcord,to='ndc'),
+            grconvertY(0,from=ycord,to='ndc'),
+            grconvertY(1,from=ycord,to='ndc'))
+
+  ypos=grconvertY(plt_ndc[[4]]-yin, from = "ndc", to = "user")
   if(align=='left') {
-    xpos=grconvertX(fig[[1]]+xin, from = "ndc", to = "user")
-    text(xpos,ypos,label,pos=4,xpd=NA,offset=0)
+    xpos=grconvertX(plt_ndc[[1]]+xin, from = "ndc", to = "user")
+    text(xpos,ypos,label,xpd=NA,adj=c(0,1))
   } else if(align=='right') {
-    xpos=grconvertX(fig[[2]]-xin, from = "ndc", to = "user")
-    text(xpos,ypos,label,pos=2,xpd=NA,offset=0)
+    xpos=grconvertX(plt_ndc[[2]]-xin, from = "ndc", to = "user")
+    text(xpos,ypos,label,xpd=NA,adj=c(1,1))
   } else {
     stop('Align must be one of left / right')
   }
