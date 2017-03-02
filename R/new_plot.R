81f1010de116c9424f0f1cdc865455f988ffff2f
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-03-02 12:56:28 +0000

    Move pretty plot to new_plot.R

--- R/new_plot.R
+++ R/new_plot.R
@@ -6,3 +6,17 @@ new_plot <- function(xlim,ylim) {
   plot.new()
   plot.window(xlim=xlim,ylim=ylim)
 }
+
+
+#' Makes a new plot with pretty axes
+#'
+#' Makes a new plot with \code{\link{new_plot}} and a bounding box and axesusing \code{\link{pretty_axes}}
+#' @inheritParams new_plot
+#' @inheritParams pretty_axes
+#' @export
+pretty_plot <- function(xlim,ylim, x_axis=1, y_axis=2, frac=FALSE,div=1,ensureZero=TRUE) {
+  new_plot(xlim,ylim)
+  if(xlim[[2]]<xlim[[1]]) xlim=rev(xlim)
+  if(ylim[[2]]<ylim[[1]]) ylim=rev(ylim)
+  pretty_axes(c(xlim[[1]],ylim[[1]]),c(xlim[[2]],ylim[[2]]),x_axis,y_axis,frac,div,ensureZero)
+}
--- R/tick_interval.R
+++ R/tick_interval.R
@@ -78,16 +78,3 @@ pretty_axes <- function(min,max, x_axis=1, y_axis=2, frac=FALSE,div=1,ensureZero
     draw_axis(min[[2]],max[[2]],y_axis,frac,div[[2]],ensureZero)
   }
 }
-
-#' Makes a new plot with pretty axes
-#'
-#' Makes a new plot with \code{\link{new_plot}} and a bounding box and axesusing \code{\link{pretty_axes}}
-#' @inheritParams new_plot
-#' @inheritParams pretty_axes
-#' @export
-pretty_plot <- function(xlim,ylim, x_axis=1, y_axis=2, frac=FALSE,div=1,ensureZero=TRUE) {
-  new_plot(xlim,ylim)
-  if(xlim[[2]]<xlim[[1]]) xlim=rev(xlim)
-  if(ylim[[2]]<ylim[[1]]) ylim=rev(ylim)
-  pretty_axes(c(xlim[[1]],ylim[[1]]),c(xlim[[2]],ylim[[2]]),x_axis,y_axis,frac,div,ensureZero)
-}
