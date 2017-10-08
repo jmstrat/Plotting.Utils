1c199b622f2aada387922960e44a223a182cb12b
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-10-08 12:10:58 +0100

    Fix printing for extended jms.data.object

--- R/jms.data.object.R
+++ R/jms.data.object.R
@@ -73,7 +73,7 @@ print.jms.data.object <- function (x, ..., digits = NULL, quote = FALSE, right =
       if(i=='jms.data.object') break
       classes_=append(classes_,i)
     }
-    extendedby = if(length(classes_)) gsub("\\.", " ", paste0('(extended by ',classes_,') ', collapse=', ')) else ''
+    extendedby = if(length(classes_)) gsub("\\.", " ", paste0('(extended by ',paste0(classes_, collapse=', '),') ')) else ''
     cat(sprintf(ngettext(n, "JMS data object %swith 0 columns and %d row",
                          "JMS data object %swith 0 columns and %d rows"), extendedby,n), "\n",
         sep = "")
