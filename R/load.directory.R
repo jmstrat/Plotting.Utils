99425dcd98023ec128ac967952e4c97433d54bdf
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-10-13 13:51:07 +0100

    Add support for loading files/directories and combining data objects

---
+++ R/load.directory.R
@@ -0,0 +1,35 @@
+#' Read every file in a directory
+#'
+#' This function reads every .ext file in a directory
+#' @param dir The path to the directory
+#' @param func The loader function
+#' @param ext The file extension to look for
+#' @param pattern The pattern to look for
+#' @param sort If TRUE then use a "natural" numerical sort (see \code{\link[Plotting.Utils]{list.files.sorted}}), else use the default alphabetical sort
+#' @details One of ext or pattern must be specified
+#' @return A list containing the data
+#' @examples
+#' load_dir_as_list('/path/to/directory',<function>,ext='ext')
+#' @keywords internal
+#' @seealso \code{\link{load_directory}} \code{\link{list.files.sorted}}
+load.directory <- function(dir,func, ext=NULL,pattern=NULL, sort=FALSE) {
+  if(is.null(pattern)) {
+    if(is.null(ext)) stop('One of ext or pattern must be specified')
+    pattern=paste0('.*\\.',ext,'$')
+  }
+  sorter = if(sort) list.files.sorted else list.files
+  file_list <- sorter(path=dir,full.names=TRUE,pattern=pattern)
+  data=lapply(file_list,func)
+}
+
+#' list.files with sorting
+#'
+#' @param ... Parameters passed to \code{\link{list.files}}
+#' @section Sorting:
+#' Natural sorting will take a file name of the sort <string><number><string>.<extension> and sort based on the number in a natural manner that does not depend on the number of digits in that number.
+#' i.e 10 would come after 9 in a natural sort, but after 1 using the default sorting method
+#' @export
+list.files.sorted <- function(...) {
+  files=list.files(...)
+  files[order(as.numeric(sub("^[^0-9]*([0-9]*).*", "\\1", basename(files))))]
+}
---
+++ R/load.jms.R
@@ -0,0 +1,59 @@
+#' Read data files
+#'
+#' This function reads one or more data files in a directory
+#' @param path The path to the file / directory
+#' @param func The function to read the file
+#' @param ... Additional parameters are passed to \code{\link[Plotting.Utils]{load.directory}} or \code{\link[Plotting.Utils]{load.file}}
+#' @return A jms.data.object containing the data
+#' @examples
+#' load.jms('/path/to/directory', load_function, ext='ext')
+#' load.jms('/path/to/file.ext', load_function)
+#' @export
+load.jms <- function(path,func,...) {
+  dat=c()
+  for(p in path) {
+    if(dir.exists(p)) dat=c(dat,load.directory(p,func,...))
+    else if(file.exists(p)) dat=c(dat,list(load.file(p,func,...)))
+    else stop(p, ' not found')
+  }
+  load.combine(as.list(dat))
+}
+
+#' Combine a list of data objects
+#'
+#' This function combines jms.data.objects
+#' @return A jms.data.object containing the data
+#' @examples
+#' load.combine(objects)
+#' @export
+load.combine <- function(objects) {
+  if(is.jms.data.object(objects)) return(objects)
+  if(!inherits(objects,'list')) stop('objects is not a list')
+  if(length(objects)==1) return(objects[[1]])
+  #Now we have to read each object as a subsequent column for a data frame
+  columns=c()
+  len_f=length(objects)
+  x_1=NA
+  for(f in 1:len_f) {
+    x_column=attr(objects[[f]],'x_column')
+    if(is.null(x_column)) x_column=1
+    x=objects[[f]][,x_column]
+    if(f==1) x_1=x
+    if(!all(x==x_1)) stop("X axis is not the same across all files -- aborting")
+    y_column=attr(objects[[f]],'y_column')
+    if(is.null(y_column)) y_column=2
+    columns[[f]]=objects[[f]][,y_column]
+  }
+  df=jms.data.object(x_1,columns)
+  #Add default attributes
+  attr(df,'x_type')<-attr(objects[[1]],'x_type')
+  attr(df,'y_type')<-attr(objects[[1]],'y_type')
+  attr(df,'x_column')<-1
+  attr(df,'y_column')<-c(2:ncol(df))
+  attr(df,'file_type')<-attr(objects[[1]],'file_type')
+  attr(df,'data_type')<-attr(objects[[1]],'data_type')
+  #Rename columns
+  names(df) <- c(attr(df,'x_type'),paste0(attr(df,'y_type'),'_',c(1:len_f)))
+  #return data
+  return(df)
+}
