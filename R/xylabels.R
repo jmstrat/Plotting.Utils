4af7c567b707c41bcd77571fa8e31e492c25e602
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-10-17 18:46:05 +0100

    Methods to get and set x and y labels

---
+++ R/xylabels.R
@@ -0,0 +1,55 @@
+#' Get or set the x or y column labels for a data object
+#'
+#' @param x The data object
+#' @return The column labels
+#' @rdname xylabels.jms
+#' @export
+xlab <- function(x) UseMethod("xlab")
+#' @export
+xlab.default <- function(x) {
+  stop("Unable to get x data for this class")
+}
+#' @export
+xlab.jms.data.object <- function(x) {
+  xlab=attr(x,'x_type')
+  if(is.null(xlab)) xlab='Unknown'
+  xlab
+}
+#' @rdname xylabels.jms
+#' @export
+`xlab<-` <- function(x,value) UseMethod("xlab<-")
+#' @export
+`xlab<-.default` <- function(x,value) {
+  stop("Unable to assign x column for this class")
+}
+#' @export
+`xlab<-.jms.data.object` <- function(x,value) {
+  attr(x,'x_type')<-value
+  x
+}
+
+#' @rdname xylabels.jms
+#' @export
+ylab <- function(x) UseMethod("ylab")
+#' @export
+ylab.default <- function(x) {
+  stop("Unable to get y data for this class")
+}
+#' @export
+ylab.jms.data.object <- function(x) {
+  ylab=attr(x,'y_type')
+  if(is.null(ylab)) ylab='Unknown'
+  ylab
+}
+#' @rdname xylabels.jms
+#' @export
+`ylab<-` <- function(x,value) UseMethod("ylab<-")
+#' @export
+`ylab<-.default` <- function(x,value) {
+  stop("Unable to assign y column for this class")
+}
+#' @export
+`ylab<-.jms.data.object` <- function(x,value) {
+  attr(x,'y_type')<-value
+  x
+}
