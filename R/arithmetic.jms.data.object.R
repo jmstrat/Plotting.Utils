db66c5dfe47019e02e3dbced0f4799fe2f110735
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-10-19 18:27:53 +0100

    Arithmetic operators only act on y data

---
+++ R/arithmetic.jms.data.object.R
@@ -0,0 +1,47 @@
+#' Add a number to a data objects y values
+#'
+#' @param e1 The data object
+#' @param e2 The number to add
+#' @return The data object
+#' @rdname arithmetic.jms
+#' @export
+`+.jms.data.object` <- function(e1, e2){
+  e1[,ycol(e1)]<-.Primitive("+")(as.data.frame(e1)[,ycol(e1)],e2)
+  e1
+}
+
+#' Subtract a number to a data objects y values
+#'
+#' @param e1 The data object
+#' @param e2 The number to subtract
+#' @return The data object
+#' @rdname arithmetic.jms
+#' @export
+`-.jms.data.object` <- function(e1, e2){
+  e1[,ycol(e1)]<-.Primitive("-")(as.data.frame(e1)[,ycol(e1)],e2)
+  e1
+}
+
+#' Multiply a data objects y values by a number
+#'
+#' @param e1 The data object
+#' @param e2 The number by which to multiply
+#' @return The data object
+#' @rdname arithmetic.jms
+#' @export
+`*.jms.data.object` <- function(e1, e2){
+  e1[,ycol(e1)]<-.Primitive("*")(as.data.frame(e1)[,ycol(e1)],e2)
+  e1
+}
+
+#' Divide a data objects y values by a number
+#'
+#' @param e1 The data object
+#' @param e2 The number by which to divide
+#' @return The data object
+#' @rdname arithmetic.jms
+#' @export
+`/.jms.data.object` <- function(e1, e2){
+  e1[,ycol(e1)]<-.Primitive("/")(as.data.frame(e1)[,ycol(e1)],e2)
+  e1
+}
