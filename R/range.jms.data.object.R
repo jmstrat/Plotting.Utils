05cf372dfc7cac96157af3c97253cd0b804f13cc
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-10-24 23:11:23 +0100

    Add offset to range

--- R/range.jms.data.object.R
+++ R/range.jms.data.object.R
@@ -7,7 +7,12 @@
 #' @examples
 #' range(data)
 #' @export
-range.jms.data.object <- function(x,...) {
+range.jms.data.object <- function(x,offset=0,...) {
   if(!is.jms.data.object(x)) stop("x must be a jms.data.object")
+  yc=ycol(x)
+  if(offset>0) {
+    ym=max(x[,ycol(x)])
+    for(i in 1:length(yc)) x[,yc[[i]]]=x[,yc[[i]]]+offset*(i-1)*ym
+  }
   range.default(x[,ycol(x)],na.rm=TRUE)
 }
