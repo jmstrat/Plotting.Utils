ebce35495c9777c9f99fcbe0d7ecc2ab50eae989
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-10-25 15:34:29 +0100

    Export attributes which are tables properly

--- R/export.R
+++ R/export.R
@@ -18,7 +18,11 @@ export.jms.data.object <- function(x,path) {
   attNames=attNames[!attNames %in% c("names","row.names","class",'y_type','x_type','x_column','y_column')]
   f <- file(path, "w")
   for(att in attNames) {
-    writeLines(paste(att, atts[[att]],sep=','),f)
+    if(inherits(atts[[att]],'data.frame')) {
+      writeLines(paste0(att,':'),f)
+      write.table(atts[[att]],f,sep=",",row.names =FALSE)
+    } else
+      writeLines(paste(att, atts[[att]],sep=','),f)
   }
   write.table(x,f,sep=",",row.names =FALSE)
   close(f)
