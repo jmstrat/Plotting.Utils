f0a0c9cd724bf16609c1866a6fe78b5f9b73d2ae
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-11-08 16:30:04 +0000

    Add interactive grid layout

--- R/grid.layout.R
+++ R/grid.layout.R
@@ -3,27 +3,32 @@
 #' @param n number of plots
 #' @param show Show the resultant layout using \code{\link{layout.show}}?
 #' @export
-grid.layout <- function(n, show=FALSE) {
+#' @rdname grid.layout
+grid.layout <- function(n, show=FALSE,layoutFunction=graphics::layout,showFunction=graphics::layout.show) {
   if(n==1) {
-    layout(1)
+    layoutFunction(1)
   } else if(n==2) {
-    layout(matrix(c(1,2),byrow=T))
+    layoutFunction(matrix(c(1,2),byrow=T))
   } else if(n==3) {
-    layout(matrix(c(1,1,2,2,0,3,3,0),2,4,byrow=T))
+    layoutFunction(matrix(c(1,1,2,2,0,3,3,0),2,4,byrow=T))
   } else if(n==4) {
-    layout(matrix(c(1,2,3,4),2,2,byrow=T))
+    layoutFunction(matrix(c(1,2,3,4),2,2,byrow=T))
   } else if(n==5) {
-    layout(matrix(c(1,1,2,2,3,3,0,4,4,5,5,0),2,6,byrow=T))
+    layoutFunction(matrix(c(1,1,2,2,3,3,0,4,4,5,5,0),2,6,byrow=T))
   } else if(n==6) {
-    layout(matrix(c(1,2,3,4,5,6),2,3,byrow=T))
+    layoutFunction(matrix(c(1,2,3,4,5,6),2,3,byrow=T))
   } else if(n==7) {
-    layout(matrix(c(1,2,3,4,5,6,0,7,0),3,3,byrow=T))
+    layoutFunction(matrix(c(1,2,3,4,5,6,0,7,0),3,3,byrow=T))
   } else if(n==8) {
-    layout(matrix(c(1,1,2,2,3,3,4,4,5,5,6,6,0,7,7,8,8,0),3,6,byrow=T))
+    layoutFunction(matrix(c(1,1,2,2,3,3,4,4,5,5,6,6,0,7,7,8,8,0),3,6,byrow=T))
   } else if(n==9) {
-    layout(matrix(c(1,2,3,4,5,6,7,8,9),3,3,byrow=T))
+    layoutFunction(matrix(c(1,2,3,4,5,6,7,8,9),3,3,byrow=T))
   } else {
     stop('Unsupported number of plots passed to grid.layout')
   }
-  if(show) layout.show(n)
+  if(show) showFunction(n)
 }
+
+#' @export
+#' @rdname grid.layout
+igrid.layout <- function(n,show=FALSE) grid.layout(n,show=show, layoutFunction=ilayout,showFunction=ilayout.show)
