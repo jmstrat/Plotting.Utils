92a74af485a886b2e35969768ca3a8b0ed615a54
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2017-12-01 18:50:41 +0000

    Move to jms.classes

--- R/expand_args.R
+++
@@ -1,6 +0,0 @@
-#' @export
-expand_args <- function(...){
-  dots <- list(...)
-  max_length <- max(sapply(dots, length))
-  lapply(dots, rep, length.out = max_length)
-}
--- R/findMethod.R
+++
@@ -1,6 +0,0 @@
-findMethod <- function(generic, ...) {
-  ch <- deparse(substitute(generic))
-  f <- X <- function(x, ...) UseMethod("X")
-  for(m in methods(ch)) assign(sub(ch, "X", m), "body<-"(f, value = m))
-  X(...)
-}
