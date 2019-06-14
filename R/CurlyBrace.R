e339a464525becd64e8d3747af3190603fedbc67
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 11:28:45 +0100

    lint

--- R/CurlyBrace.R
+++ R/CurlyBrace.R
@@ -6,30 +6,29 @@
 #' @param height Height of the brace in inches
 #' @param ... Additional parameters passed to \code{\link{lines}}
 #' @export
-CurlyBrace <- function(start, end, width,height,...) {
-  width=xinch(width)
-  height=yinch(height)
+CurlyBrace <- function(start, end, width, height, ...) {
+  width <- xinch(width)
+  height <- yinch(height)
   # Transform from axis to screen coordinates
-  th = atan2(end[[2]]-start[[2]], end[[1]]-start[[1]])
-  c1 = start + width*c(cos(th), sin(th))
-  c2 = 0.5*(start+end) + 2*c(-width*sin(th), height*cos(th)) - c(width*cos(th), height*sin(th))
-  c3 = 0.5*(start+end) + 2*c(-width*sin(th), height*cos(th)) + c(width*cos(th), height*sin(th))
-  c4 = end - width*c(cos(th), sin(th))
+  th <- atan2(end[[2]] - start[[2]], end[[1]] - start[[1]])
+  c1 <- start + width * c(cos(th), sin(th))
+  c2 <- 0.5 * (start + end) + 2 * c(-width * sin(th), height * cos(th)) - c(width * cos(th), height * sin(th))
+  c3 <- 0.5 * (start + end) + 2 * c(-width * sin(th), height * cos(th)) + c(width * cos(th), height * sin(th))
+  c4 <- end - width * c(cos(th), sin(th))
 
   # Assemble brace coordinates
-  q = seq(0+th, pi/2+th, length=50)
-  t = q[length(q):1]
-  part1x = width*cos(t+pi/2) + c1[[1]]
-  part1y = height*sin(t+pi/2) + c1[[2]]
-  part2x = width*cos(q-pi/2) + c2[[1]]
-  part2y = height*sin(q-pi/2) + c2[[2]]
-  part3x = width*cos(q+pi) + c3[[1]]
-  part3y = height*sin(q+pi) + c3[[2]]
-  part4x = width*cos(t) + c4[[1]]
-  part4y = height*sin(t) + c4[[2]]
-  x = c(part1x, part2x, part3x, part4x)
-  y = c(part1y, part2y, part3y, part4y)
+  q <- seq(0 + th, pi / 2 + th, length=50)
+  t <- q[length(q):1]
+  part1x <- width * cos(t + pi / 2) + c1[[1]]
+  part1y <- height * sin(t + pi / 2) + c1[[2]]
+  part2x <- width * cos(q - pi / 2) + c2[[1]]
+  part2y <- height * sin(q - pi / 2) + c2[[2]]
+  part3x <- width * cos(q + pi) + c3[[1]]
+  part3y <- height * sin(q + pi) + c3[[2]]
+  part4x <- width * cos(t) + c4[[1]]
+  part4y <- height * sin(t) + c4[[2]]
+  x <- c(part1x, part2x, part3x, part4x)
+  y <- c(part1y, part2y, part3y, part4y)
 
-  lines(x, y,...)
-
+  lines(x, y, ...)
 }
