b144818eb20b77880d9d9e4dd38efc56acdc388b
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 11:32:47 +0100

    lint

--- R/dyUtils.R
+++ R/dyUtils.R
@@ -1,19 +1,24 @@
 #' Add a box to a \code{\link[dygraphs]{dygraph}}
 #'
 #' @export
-dyBox <- function(dygraph,lwd=1) {
-  dygraphs::dyCallbacks(dygraph,underlayCallback=htmlwidgets::JS(paste0("function(ctx, area, dygraph) {
-                                                                        ctx.lineWidth=",lwd*2,";
-                                                                        ctx.strokeStyle = 'black';
-                                                                        ctx.strokeRect(area.x, area.y, area.w, area.h);
-}")))
+dyBox <- function(dygraph, lwd=1) {
+  dygraphs::dyCallbacks(
+    dygraph,
+    underlayCallback=htmlwidgets::JS(
+      paste0("function(ctx, area, dygraph) {
+                 ctx.lineWidth=", lwd * 2, ";
+                 ctx.strokeStyle = 'black';
+                 ctx.strokeRect(area.x, area.y, area.w, area.h);
+              }")
+    )
+  )
 }
 
 #' Set the x limits for a \code{\link[dygraphs]{dygraph}}
 #'
 #' @export
-dyxlim <- function(dygraph,xlim) {
-  dygraph$x$attrs$dateWindow<-xlim
+dyxlim <- function(dygraph, xlim) {
+  dygraph$x$attrs$dateWindow <- xlim
   dygraph
 }
 
@@ -23,38 +28,49 @@ dyxlim <- function(dygraph,xlim) {
 #' @param digits Number of decimal places in legend (only relevant if ticks==TRUE)
 #'
 #' @export
-dyAxis.jms <- function(graph,name,label,valueRange,ticks=TRUE,digits=2) {
-  graph<- if(ticks)
-    dygraphs::dyAxis(graph,name,label=label,valueRange=valueRange, drawGrid = FALSE,axisLineWidth = 1,independentTicks=TRUE,
-                   valueFormatter=paste0('function(d){return parseFloat(Math.round(d*100)/100).toFixed(',digits,')}'))
-  else
-    dygraphs::dyAxis(graph,name,label=label,valueRange=valueRange, drawGrid = FALSE,axisLineWidth = 1,
-                     axisLabelWidth=20,ticker='function(x) {return[]}',
-                     valueFormatter=' function(x) {return("")}')
+dyAxis.jms <- function(graph, name, label, valueRange, ticks=TRUE, digits=2) {
+  graph <- if (ticks) {
+    dygraphs::dyAxis(graph, name,
+      label=label, valueRange=valueRange, drawGrid=FALSE, axisLineWidth=1, independentTicks=TRUE,
+      valueFormatter=paste0("function(d) {return parseFloat(Math.round(d * 100) / 100).toFixed(", digits, ")}")
+    )
+  } else {
+    dygraphs::dyAxis(graph, name,
+      label=label, valueRange=valueRange, drawGrid=FALSE, axisLineWidth=1,
+      axisLabelWidth=20, ticker="function(x) {return[]}",
+      valueFormatter='function(x) {return("")}'
+    )
+  }
 
-  if(name=='x') graph<-dyxlim(graph,valueRange)
+  if (name == "x") graph <- dyxlim(graph, valueRange)
   graph
 }
 
 #' Sets the visibility of a trace
 #' @export
-dyVisibility <- function (graph, trace=1, visibility = TRUE){
+dyVisibility <- function(graph, trace=1, visibility=TRUE) {
   vis <- graph$x$attrs$visibility
-  if(!length(vis)) vis <- rep_len(TRUE, length(graph$x$data))
-  vis[trace]<-visibility
-  graph$x$attrs$visibility<-vis
+  if (!length(vis)) vis <- rep_len(TRUE, length(graph$x$data))
+  vis[trace] <- visibility
+  graph$x$attrs$visibility <- vis
   graph
 }
 
 #' Create a blank plot
 #' @export
 dyBlankPlot <- function() {
-  blank_plot<- dygraphs::dygraph(data.frame(x=0.5,y=NA))
-  blank_plot<-dygraphs::dyOptions(blank_plot,drawXAxis=F,drawYAxis=F,drawGrid=F,pointSize=0,
-                                  plotter='function(p) {return}',strokeWidth=0)
-  blank_plot<-dygraphs::dyCallbacks(blank_plot,drawPointCallback='function(p) {return}',annotationMouseOverHandler='function(p) {return}')
-  blank_plot<-Plotting.Utils::dyxlim(blank_plot,c(0,1))
-  blank_plot<-dygraphs::dyAxis(blank_plot,'y',valueRange=c(0,1))
-  blank_plot<-dygraphs::dyLegend(blank_plot,show='never')
+  blank_plot <- dygraphs::dygraph(data.frame(x=0.5, y=NA))
+  blank_plot <- dygraphs::dyOptions(blank_plot,
+    drawXAxis=F, drawYAxis=F, drawGrid=F, pointSize=0,
+    plotter="function(p) {return}", strokeWidth=0
+  )
+  blank_plot <- dygraphs::dyCallbacks(
+    blank_plot,
+    drawPointCallback="function(p) {return}",
+    annotationMouseOverHandler="function(p) {return}"
+  )
+  blank_plot <- Plotting.Utils::dyxlim(blank_plot, c(0, 1))
+  blank_plot <- dygraphs::dyAxis(blank_plot, "y", valueRange=c(0, 1))
+  blank_plot <- dygraphs::dyLegend(blank_plot, show="never")
   blank_plot
 }
