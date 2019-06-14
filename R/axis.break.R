af7b1180d7482465698dd5dd1b95c783ba171c19
Josh Stratford <51674722+jmstrat@users.noreply.github.com>
2019-06-14 11:19:14 +0100

    lint

--- R/axis.break.R
+++ R/axis.break.R
@@ -5,33 +5,34 @@
 #' @param breakpos If only one axis is provided, then a single number corresponding to the point at which to break the axis, otherwise a vector of two numbers c(x,y)
 #' @param brw If only one axis is provided, then a single number corresponding to the width of the break as a fraction of the total width, otherwise a vector of two numbers c(x,y)
 #' @export
-axis.break.box <- function (axis = 1, breakpos, brw = 0.02)
-{
+axis.break.box <- function(axis=1, breakpos, brw=0.02) {
   figxy <- par("usr")
   xaxl <- par("xlog")
   yaxl <- par("ylog")
 
-  xa=axis %in% c(1,3)
-  if(any(xa)) {
+  xa <- axis %in% c(1, 3)
+  if (any(xa)) {
     breakposx <- breakpos[xa]
-    brwx=brw
-    if(length(brw)==2) brwx=brw[xa]
+    brwx <- brw
+    if (length(brw) == 2) brwx <- brw[xa]
     xw <- (figxy[2] - figxy[1]) * brwx
-    if(xaxl)
+    if (xaxl) {
       breakposx <- log10(breakposx)
+    }
   }
-  ya=axis %in% c(2,4)
-  if(any(ya)) {
+  ya <- axis %in% c(2, 4)
+  if (any(ya)) {
     breakposy <- breakpos[ya]
-    brwy=brw
-    if(length(brw)==2) brwy=brw[ya]
+    brwy <- brw
+    if (length(brw) == 2) brwy <- brw[ya]
     yw <- (figxy[4] - figxy[3]) * brwy
-    if(yaxl)
+    if (yaxl) {
       breakposy <- log10(breakposy)
+    }
   }
 
   old.xpd <- par("xpd")
-  par(xpd = TRUE)
+  par(xpd=TRUE)
 
   if (xaxl) {
     figxy[1] <- 10^figxy[1]
@@ -42,16 +43,18 @@ axis.break.box <- function (axis = 1, breakpos, brw = 0.02)
     figxy[4] <- 10^figxy[4]
   }
   if (any(xa)) {
-    #Fill with white
-    rect(breakposx-xw/2, figxy[3], breakposx + xw/2, figxy[4],
-         col = 'white', border = 'white')
-    if(any(ya)) {
-      rect(figxy[1], breakposy-yw/2, figxy[2], breakposy + yw/2,
-           col = 'white', border = 'white')
-      xbegin <- c(figxy[1],figxy[1],breakposx + xw/2,breakposx + xw/2)
-      ybegin <- c(figxy[3], breakposy + yw/2,breakposy + yw/2,figxy[3])
-      xend <- c(breakposx-xw/2,breakposx-xw/2, figxy[2], figxy[2])
-      yend <- c(breakposy - yw/2, figxy[4], figxy[4],breakposy - yw/2)
+    # Fill with white
+    rect(breakposx - xw / 2, figxy[3], breakposx + xw / 2, figxy[4],
+      col="white", border="white"
+    )
+    if (any(ya)) {
+      rect(figxy[1], breakposy - yw / 2, figxy[2], breakposy + yw / 2,
+        col="white", border="white"
+      )
+      xbegin <- c(figxy[1], figxy[1], breakposx + xw / 2, breakposx + xw / 2)
+      ybegin <- c(figxy[3], breakposy + yw / 2, breakposy + yw / 2, figxy[3])
+      xend <- c(breakposx - xw / 2, breakposx - xw / 2, figxy[2], figxy[2])
+      yend <- c(breakposy - yw / 2, figxy[4], figxy[4], breakposy - yw / 2)
       if (xaxl) {
         xbegin <- 10^xbegin
         xend <- 10^xend
@@ -61,9 +64,9 @@ axis.break.box <- function (axis = 1, breakpos, brw = 0.02)
         yend <- 10^xend
       }
     } else {
-      xbegin <- c(figxy[1],breakposx + xw/2)
+      xbegin <- c(figxy[1], breakposx + xw / 2)
       ybegin <- c(figxy[3], figxy[3])
-      xend <- c(breakposx-xw/2, figxy[2])
+      xend <- c(breakposx - xw / 2, figxy[2])
       yend <- c(figxy[4], figxy[4])
       if (xaxl) {
         xbegin <- 10^xbegin
@@ -71,21 +74,22 @@ axis.break.box <- function (axis = 1, breakpos, brw = 0.02)
       }
     }
   }
-  else {  #Just y axis
-    rect(figxy[1], breakposy-yw/2, figxy[2], breakposy + yw/2,
-         col = 'white', border = 'white')
+  else { # Just y axis
+    rect(figxy[1], breakposy - yw / 2, figxy[2], breakposy + yw / 2,
+      col="white", border="white"
+    )
     xbegin <- c(figxy[1], figxy[1])
-    ybegin <- c(figxy[3], breakposy + yw/2)
+    ybegin <- c(figxy[3], breakposy + yw / 2)
     xend <- c(figxy[2], figxy[2])
-    yend <- c(breakposy-yw/2, figxy[4])
+    yend <- c(breakposy - yw / 2, figxy[4])
     if (yaxl) {
       ybegin <- 10^xbegin
       yend <- 10^xend
     }
   }
-  par(xpd = TRUE)
-  rect(xbegin, ybegin, xend, yend, border = 'black', lty = 1)
-  par(xpd = FALSE)
+  par(xpd=TRUE)
+  rect(xbegin, ybegin, xend, yend, border="black", lty=1)
+  par(xpd=FALSE)
 }
 
 #' Put a box around a plot, leaving gaps for axis breaks on the x and / or y axes
@@ -98,13 +102,13 @@ axis.break.box <- function (axis = 1, breakpos, brw = 0.02)
 #' @param brwy As brw, but specifically in the y direction (do not specify both brw and brwx/y)
 #' @export
 #' @seealso \code{\link{axis.break.box}}
-axis.break.slash <- function (axis = 1, breakpos, brw = 0.02, brwx=NA, brwy=NA)
-{
-  if(all(is.na(c(brwx,brwy)))) brwx=brwy=brw
-  if(length(axis)>1 || length(breakpos)>1) {
-    args=expand_args(axis=axis,breakpos=breakpos,brwx=brwx,brwy=brwy)
-    for(i in 1:length(args$axis))
-      axis.break.slash(axis=args$axis[[i]],breakpos=args$breakpos[[i]],brwx=args$brwx[[i]],brwy=args$brwy[[i]])
+axis.break.slash <- function(axis=1, breakpos, brw=0.02, brwx=NA, brwy=NA) {
+  if (all(is.na(c(brwx, brwy)))) brwx <- brwy <- brw
+  if (length(axis) > 1 || length(breakpos) > 1) {
+    args <- expand_args(axis=axis, breakpos=breakpos, brwx=brwx, brwy=brwy)
+    for (i in 1:length(args$axis)) {
+      axis.break.slash(axis=args$axis[[i]], breakpos=args$breakpos[[i]], brwx=args$brwx[[i]], brwy=args$brwy[[i]])
+    }
     return(invisible())
   }
   figxy <- par("usr")
@@ -113,37 +117,50 @@ axis.break.slash <- function (axis = 1, breakpos, brw = 0.02, brwx=NA, brwy=NA)
   xw <- (figxy[2] - figxy[1]) * brwx
   yw <- (figxy[4] - figxy[3]) * brwy
 
-  if (xaxl && (axis == 1 || axis == 3))
+  if (xaxl && (axis == 1 || axis == 3)) {
     breakpos <- log10(breakpos)
-  if (yaxl && (axis == 2 || axis == 4))
+  }
+  if (yaxl && (axis == 2 || axis == 4)) {
     breakpos <- log10(breakpos)
+  }
   switch(axis,
-         br <- c(breakpos - xw/2,
-                 figxy[3] - yw/2,
-                 breakpos + xw/2,
-                 figxy[3] + yw/2),
-         br <- c(figxy[1] - xw/2,
-                 breakpos - yw/2,
-                 figxy[1] + xw/2,
-                 breakpos + yw/2),
-         br <- c(breakpos - xw/2,
-                 figxy[4] - yw/2,
-                 breakpos + xw/2,
-                 figxy[4] + yw/2),
-         br <- c(figxy[2] - xw/2,
-                 breakpos - yw/2,
-                 figxy[2] + xw/2,
-                 breakpos + yw/2),
-         stop("Improper axis specification."))
+    br <- c(
+      breakpos - xw / 2,
+      figxy[3] - yw / 2,
+      breakpos + xw / 2,
+      figxy[3] + yw / 2
+    ),
+    br <- c(
+      figxy[1] - xw / 2,
+      breakpos - yw / 2,
+      figxy[1] + xw / 2,
+      breakpos + yw / 2
+    ),
+    br <- c(
+      breakpos - xw / 2,
+      figxy[4] - yw / 2,
+      breakpos + xw / 2,
+      figxy[4] + yw / 2
+    ),
+    br <- c(
+      figxy[2] - xw / 2,
+      breakpos - yw / 2,
+      figxy[2] + xw / 2,
+      breakpos + yw / 2
+    ),
+    stop("Improper axis specification.")
+  )
   old.xpd <- par("xpd")
-  par(xpd = TRUE)
-  if (xaxl)
+  par(xpd=TRUE)
+  if (xaxl) {
     br[c(1, 3)] <- 10^br[c(1, 3)]
-  if (yaxl)
+  }
+  if (yaxl) {
     br[c(2, 4)] <- 10^br[c(2, 4)]
+  }
 
-  #Remove bit of axis where break mark will go
-  rect(br[1], br[2], br[3], br[4], col = 'white', border = 'white')
+  # Remove bit of axis where break mark will go
+  rect(br[1], br[2], br[3], br[4], col="white", border="white")
   if (axis == 1 || axis == 3) {
     xbegin <- c(breakpos - xw, breakpos)
     xend <- c(breakpos, breakpos + xw)
@@ -164,6 +181,6 @@ axis.break.slash <- function (axis = 1, breakpos, brw = 0.02, brwx=NA, brwy=NA)
       yend <- 10^yend
     }
   }
-  segments(xbegin, ybegin, xend, yend, col = 'black', lty = 1)
-  par(xpd = FALSE)
+  segments(xbegin, ybegin, xend, yend, col="black", lty=1)
+  par(xpd=FALSE)
 }
